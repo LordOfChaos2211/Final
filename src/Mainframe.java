c4c36a79c22d6c7274b2c345a28109608b0090aa
LordOfChaos2211 <mofmof2211@gmail.com>
2024-05-23 20:10:10 +0300

    mainframe updated and imported jfreechart library

--- src/Mainframe.java
+++ src/Mainframe.java
@@ -3,9 +3,31 @@ import java.awt.*;
 
 public class Mainframe extends JFrame {
     JFrame mainframe;
+    JPanel leftPane;
+    JPanel bottomPane;
+    JPanel content;
     Mainframe(Dimension dim){
         mainframe = new JFrame();
         mainframe.setSize(dim);
+
+        leftPane = new JPanel();
+        leftPane.setPreferredSize(new Dimension((int) (mainframe.getWidth()*.2),100));
+        leftPane.setBackground(Color.RED);
+        //leftPane.setOpaque(true);
+
+        bottomPane = new JPanel();
+        bottomPane.setPreferredSize(new Dimension(100,(int) (mainframe.getHeight()*.2)));
+        bottomPane.setBackground(Color.BLUE);
+       // bottomPane.setOpaque(true);
+
+        content = new JPanel();
+        content.setBackground(Color.GREEN);
+        //content.setOpaque(true);
+
+        mainframe.setLayout(new BorderLayout());
+        mainframe.add(leftPane,BorderLayout.WEST);
+        mainframe.add(bottomPane,BorderLayout.SOUTH);
+        mainframe.add(content,BorderLayout.CENTER);
         mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainframe.setResizable(false);
         mainframe.setVisible(true);
